9041d7587536632e8505a646a57af27b21d5f3e6
Felipe LIncoln <felipelincoln@usp.br>
2021-08-25 07:22:49 -0300

    fix rolls

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 console.log("Auto roll extension is running...")
 
-document.getElementById("pwc_input").value = "1";
-document.getElementById("free_play_form_button").click()
+setTimeout(() => {
+    document.getElementById("pwc_input").value = "1";
+    document.getElementById("free_play_form_button").click()
+  }, 6000
+)
